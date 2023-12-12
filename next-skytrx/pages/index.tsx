858a764aad1be017cddc359468cdb094b65db327
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2023-12-13 04:22:28 +0900

    Bar更新

--- next-skytrx/pages/index.tsx
+++ next-skytrx/pages/index.tsx
@@ -9,6 +9,8 @@ const Home: React.FC = () => {
       <h1>Index</h1>
       <h4>root - indextest</h4>
       <a href="https://skytrx2023.vercel.app/test">test</a>
+        <br />
+      <a href="localhost:3000/test">test - dev</a>
     </div>
   );
 };
--- next-skytrx/pages/test.tsx
+++ next-skytrx/pages/test.tsx
@@ -3,6 +3,7 @@ import Button from '@mui/material/Button';
 import ResponsiveAppBar from '../components/ResponsiveAppBar.tsx';
 import BasicTabs from '../components/Tab.tsx';
 import BasicTabsB from '../components/TabB.tsx';
+import styled from 'styled-components';
 
 function getGreetingBasedOnTime() {
   const hours = new Date().getHours();
@@ -16,25 +17,34 @@ function getGreetingBasedOnTime() {
   }
 }
 
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 0 -8px;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
+
 const Test = () => {
-  const greeting = getGreetingBasedOnTime(); // 時間に基づく挨拶を取得
-
-  return (
-    <div>
-      <ResponsiveAppBar />
-      <h4>{greeting}、清水さん</h4>
-      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-        <div style={{ flex: '1 1 auto', minWidth: '50%' }}>
-          <BasicTabs />
-        </div>
-        <div style={{ flex: '1 1 auto', minWidth: '50%' }}>
-          <BasicTabsB />
-        </div>
+    return (
+      <div>
+        <ResponsiveAppBar />
+        <h4>{getGreetingBasedOnTime()}、清水さん</h4>
+        <FlexContainer>
+          <div style={{ flex: '1 1 0', padding: '0 32px', width: '100%' }}>
+            <BasicTabs />
+          </div>
+          <div style={{ flex: '1 1 0', padding: '0 32px', width: '100%' }}>
+            <BasicTabsB />
+          </div>
+        </FlexContainer>
+        <h1>Test</h1>
+        <Button variant="contained">Test</Button>
       </div>
-      <h1>Test</h1>
-      <Button variant="contained">Test</Button>
-    </div>
-  );
-};
-
-export default Test;
+    );
+  };
+  
+  export default Test;
