569217fadc54634a434c20b51c80cd62da3ed0f4
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2023-12-12 21:31:55 +0900

    index

---
+++ next-skytrx/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Index</h1>
+      <h4>root - indextest</h4>
+      <h4>test</h4>
+    </div>
+  );
+};
+
+export default Home;
