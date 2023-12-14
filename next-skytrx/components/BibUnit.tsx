8ba07ed806460e6a19bda06b25b3538c34472a21
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2023-12-14 17:17:18 +0900

    BibUnitだれかできませんか

--- next-skytrx/components/BibUnit.tsx
+++ next-skytrx/components/BibUnit.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Paper, Typography, Grid, Rating } from '@mui/material';
+import users from '../public/data/users.json';
 
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import EditIcon from '@mui/icons-material/Edit';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 export default function BibUnit({ articles }) {
   return (
     <div>
@@ -11,16 +15,63 @@ export default function BibUnit({ articles }) {
               {article.title}
             </Typography>
           </a>
+          <Typography variant="h6" component="span" style={{ marginLeft: '8px' }}>
+            {article.personalid.map(id => users[id] ? users[id].name : "名前なし").join(", ")}
+          </Typography>
           <Typography variant="subtitle1">Published on: {article.year}/{article.month}/{article.day}</Typography>
-          <Grid container>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body2">Impact: {article.impact}</Typography>
-              <Rating name="impact-rating" value={parseFloat(article.impact)} readOnly />
+
+          <Grid container spacing={2}>
+            {/* 左側のコンテナ */}
+            <Grid item xs={12} sm={6} container direction="column" spacing={2}>
+              <Grid item>
+                <Grid container alignItems="center" spacing={1}>
+                  <Typography variant="h6">Impact</Typography>
+                  <Rating name="impact-rating" value={parseFloat(article.impact)} readOnly />
+                </Grid>
+                <Grid container alignItems="center" spacing={1}>
+                  <Grid item>
+                    <VisibilityIcon />
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="body2">{article.view}</Typography>
+                  </Grid>
+                </Grid>
+                <Grid container alignItems="center" spacing={1}>
+                  <Grid item>
+                    <EditIcon />
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="body2">{article.edit}</Typography>
+                  </Grid>
+                  <Grid item>
+                    <ChatBubbleIcon />
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="body2">{article.comment}</Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body2">Views: {article.view}</Typography>
+
+            {/* 右側のコンテナ */}
+            <Grid item xs={12} sm={6} container direction="column" spacing={1}>
+              <Grid item container alignItems="center" spacing={1}>
+                <Typography variant="body2">Novelty</Typography>
+                <Rating name="novelty" value={parseFloat(article.novelty)} readOnly />
+              </Grid>
+            <Grid item container alignItems="center" spacing={1}>
+                <Typography variant="body2">Originality</Typography>
+                <Rating name="originality" value={parseFloat(article.originality)} readOnly />
+              </Grid>
+              <Grid item container alignItems="center" spacing={1}>
+                <Typography variant="body2">Validity</Typography>
+                <Rating name="validity" value={parseFloat(article.validity)} readOnly />
+              </Grid>
+              <Grid item container alignItems="center" spacing={1}>
+                <Typography variant="body2">Ethics</Typography>
+                <Rating name="ethics" value={parseFloat(article.ethics)} readOnly />
+              </Grid>
             </Grid>
-            {/* 他の評価や情報も同様に表示 */}
           </Grid>
         </Paper>
       ))}
