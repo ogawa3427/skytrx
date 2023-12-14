4055b59920764b30fbc0e59c22d09c37af41e28a
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2023-12-15 01:59:28 +0900

    わからない

--- next-skytrx/components/Nbar.tsx
+++ next-skytrx/components/Nbar.tsx
@@ -10,31 +10,43 @@ import Cookies from 'js-cookie';
 import { AuthContext } from '../pages/index';
 
 export default function Navbar() {
-  const { isLoggedIn, setIsLoggedIn } = React.useContext(AuthContext); // ログイン状態を管理するための state
+  const { isLoggedIn, setIsLoggedIn } = React.useContext(AuthContext);
+
+  const handleLogout = () => {
+    Cookies.set('loggedin', 'false');
+    setIsLoggedIn(false);
+    window.location.reload();
+  };
+
+  const handleLogin = () => {
+    Cookies.set('loggedin', 'true');
+    setIsLoggedIn(true);
+    window.location.reload();
+  };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="default" style={{ backgroundColor: 'gray' }}>
-        <Toolbar>
-        <Typography variant="h4" component="div" sx={{ flexGrow: 1, color: 'white', fontFamily: 'Times New Roman, serif', textTransform: 'none' }}>
-          <Link href="/" style={{ color: 'white', textDecoration: 'none' }}>
-            Science Chain
-          </Link>
-        </Typography>
-          <Stack direction="column" spacing={1} style={{ marginTop: '8px', marginBottom: '8px' }}>
-            {isLoggedIn ? (
-              // ログインしている場合の表示
-              <Button color="inherit" style={{ backgroundColor: '#505050', color: '#FFFFFF', borderRadius: '25px', border: '2px solid #FFFFFF', textTransform: 'none', fontFamily: 'Times New Roman, serif', padding: '2px 50px' }} onClick={() => {Cookies.set('loggedin', 'false'); window.location.reload();}}>Logout</Button>
-            ) : (
-              // ログインしていない場合の表示
-              <>
-                <Button color="inherit" style={{ backgroundColor: '#505050', color: '#FFFFFF', borderRadius: '25px', border: '2px solid #FFFFFF', textTransform: 'none', fontFamily: 'Times New Roman, serif', padding: '2px 50px' }} onClick={() => {Cookies.set('loggedin', 'true'); window.location.reload();}}>Login</Button>
-                <Button color="inherit" style={{ backgroundColor: '#FFFFFF', color: '#000000', borderRadius: '25px', border: '2px solid #000000', textTransform: 'none', fontFamily: 'Times New Roman, serif', padding: '2px 50px' }}>SignUp</Button>
-              </>
-            )}
-          </Stack>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AuthContext.Provider value={{ isLoggedIn, handleLogout, handleLogin }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="default" style={{ backgroundColor: 'gray' }}>
+          <Toolbar>
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1, color: 'white', fontFamily: 'Times New Roman, serif', textTransform: 'none' }}>
+            <Link href="/" style={{ color: 'white', textDecoration: 'none' }}>
+              Science Chain
+            </Link>
+          </Typography>
+            <Stack direction="column" spacing={1} style={{ marginTop: '8px', marginBottom: '8px' }}>
+              {isLoggedIn ? (
+                <Button color="inherit" style={{ backgroundColor: '#505050', color: '#FFFFFF', borderRadius: '25px', border: '2px solid #FFFFFF', textTransform: 'none', fontFamily: 'Times New Roman, serif', padding: '2px 50px' }} onClick={handleLogout}>Logout</Button>
+              ) : (
+                <>
+                  <Button color="inherit" style={{ backgroundColor: '#505050', color: '#FFFFFF', borderRadius: '25px', border: '2px solid #FFFFFF', textTransform: 'none', fontFamily: 'Times New Roman, serif', padding: '2px 50px' }} onClick={handleLogin}>Login</Button>
+                  <Button color="inherit" style={{ backgroundColor: '#FFFFFF', color: '#000000', borderRadius: '25px', border: '2px solid #000000', textTransform: 'none', fontFamily: 'Times New Roman, serif', padding: '2px 50px' }}>SignUp</Button>
+                </>
+              )}
+            </Stack>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </AuthContext.Provider>
   );
 }
