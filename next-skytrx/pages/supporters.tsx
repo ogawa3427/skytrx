0b62f874312553c221694e02d90970cbeb966251
Taro-Haraguchi <50039825+Taro-Haraguchi@users.noreply.github.com>
2023-12-14 19:19:22 +0900

    supportersもいったぜ

--- next-skytrx/pages/supporters.tsx
+++ next-skytrx/pages/supporters.tsx
@@ -1,11 +1,11 @@
 import react from 'react';
 import styled from 'styled-components';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
-
+import Navbar from "../components/Nbar";
 const Supporters = () => {
   return (
     <div>
-      <ResponsiveAppBar />
+      <Navbar />
       <h1>Supporters</h1>
     </div>
   );
