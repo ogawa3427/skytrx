4fbbb20778fa46070b3e1eeb2e4c81295ce40d87
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2023-12-15 22:47:57 +0900

    visualabstでかお

--- next-skytrx/components/BibUnit.tsx
+++ next-skytrx/components/BibUnit.tsx
@@ -10,6 +10,7 @@ import FourStars from './FourStars';
 import Impact from './Impact';
 import ThreeIcons from './ThreeIcons';
 import WhLimit from './WhLimit';
+import LimitDate from './LimitDate';
 
 
 export default function BibUnit({ articles, status }) {
@@ -22,38 +23,56 @@ export default function BibUnit({ articles, status }) {
     setFilteredArticles(filtered);
   }, [articles, status]);
 
+  const renderEnoughStatus = (article) => (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Grid container direction="column">
+            <Grid item>
+              <Impact article={article} />
+            </Grid>
+            <Grid item>
+              <ThreeIcons article={article} />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FourStars article={article} />
+        </Grid>
+      </Grid>
+    </>
+  );
+  
+  const renderNotSubmittedReviewStatus = (article) => (
+    <>
+      <WhLimit article={article} />
+    </>
+  );
+  
+  const renderSubmittedReviewStatus = (article) => (
+    <>
+      <Impact article={article} />
+      <LimitDate article={article} />
+      <StartReview article={article} />
+    </>
+  );
+  
   return (
     <>
       {filteredArticles.map((article, index) => (
         <Paper key={index} elevation={3} sx={{ margin: '16px', padding: '16px' }}>
-          <TiAuPuEd article={article} />
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} container direction="column" spacing={2}>
-              <Grid item>
-                {(status === 'enough') ? (
-                  <>
-                    <Grid container spacing={2}>
-                      <Grid item xs={12} sm={6}>
-                        <Grid container direction="column">
-                          <Grid item>
-                            <Impact article={article} />
-                          </Grid>
-                          <Grid item>
-                            <ThreeIcons article={article} />
-                          </Grid>
-                        </Grid>
-                      </Grid>
-                      <Grid item xs={12} sm={6}>
-                        <FourStars article={article} />
-                      </Grid>
-                    </Grid>
-                  </>
-                ) : (
-                  <>
-                    <WhLimit article={article} />
-                  </>
-                )}
+            <Grid item xs={12} sm={12} container direction="row" alignItems="stretch">
+              <Grid item style={{ maxHeight: '500px' }}>
+                <img src={`/images/${article.img}.png`} alt="image" style={{ width: 'auto', height: '100%', objectFit: 'contain' }} />
               </Grid>
+              <Grid item>
+                <TiAuPuEd article={article} />
+                {status === 'enough' ? renderEnoughStatus(article) :
+                status !== 'submittedReview' ? renderNotSubmittedReviewStatus(article) :
+                renderSubmittedReviewStatus(article)}
+              </Grid>
+            
             </Grid>
           </Grid>
         </Paper>
