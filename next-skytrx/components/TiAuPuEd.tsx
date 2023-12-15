7640159c19f289d879c7a0bc528d0fed42edfbf9
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2023-12-15 17:48:02 +0900

    pathごめん

--- next-skytrx/components/TiAuPuEd.tsx
+++ next-skytrx/components/TiAuPuEd.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import users from "/public/data/users.json";
+import users from '../public/data/users.json';
 
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
